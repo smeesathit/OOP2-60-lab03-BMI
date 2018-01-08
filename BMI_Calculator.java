6a2f41503283493a607ed2c4c13ece209b2343aa
smeesathit <smeesathit.gmail.com>
2018-01-08 22:14:58 +0700

    Development step 2: calculate & show BMI Add formula in readme.txt
    Signed-off-by: smeesathit <smeesathit.gmail.com>

--- BMI_Calculator.java
+++ BMI_Calculator.java
@@ -18,16 +18,25 @@ public class BMI_Calculator
         
         // Get weight and height via the standard input window
         double w = 0.0;
-        int h = 0;
+        double h = 0;
+        double bmi;
         
         System.out.print("Enter weight in kilogram: ");
         w = scanner.nextDouble();
         System.out.print("Enter height in centimeter: ");
-        h = scanner.nextInt();
+        h = scanner.nextDouble();
         
+        /*
         // Echo the input for testing purpose
         System.out.println();
         System.out.println("weight = " + w);
         System.out.println("height = " + h);
+        */
+        
+       // Compute BMI
+        h = h / 100.0;
+        bmi = w / (h * h);
+        System.out.println("BMI = " + bmi);
+               
     } // End main
 } // End class
